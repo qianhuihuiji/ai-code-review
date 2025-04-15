871de4cbcfd32a7641b25b18b9aadf760699bb0d
梅一名 <1025926969@qq.com>
2025-04-15 17:30:01 +0800

    接入DingDing

--- src/main/java/com/nofirst/ai/code/review/service/DingDingService.java
+++ src/main/java/com/nofirst/ai/code/review/service/DingDingService.java
@@ -4,14 +4,19 @@ import com.dingtalk.api.DefaultDingTalkClient;
 import com.dingtalk.api.DingTalkClient;
 import com.dingtalk.api.request.OapiRobotSendRequest;
 import com.dingtalk.api.response.OapiRobotSendResponse;
+import com.nofirst.ai.code.review.config.DingTalkConfiguration;
 import com.taobao.api.ApiException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DingDingService {
 
+    @Autowired
+    private DingTalkConfiguration dingTalkConfiguration;
+
     public void sendMessageWebhook(String title, String content) throws ApiException {
-        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=xxx");
+        DingTalkClient client = new DefaultDingTalkClient(dingTalkConfiguration.getWebhook());
         OapiRobotSendRequest request = new OapiRobotSendRequest();
         request.setMsgtype("markdown");
         OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
